e338ed30f9ca498ebb9ff46dc4b963aa2c9183cb
usbbush <jeanbienvenusb@gmail.com>
2023-06-27 11:48:40 +0200

    Update associations

--- db/migrate/20230627080310_create_products_tags.rb
+++ db/migrate/20230627080310_create_products_tags.rb
@@ -1,10 +1,8 @@
 class CreateProductsTags < ActiveRecord::Migration[7.0]
   def change
-    create_table :products_tags do |t|
+    create_table :products_tags, id:false do |t|
       t.references :tag, null: false, foreign_key: true
       t.references :product, null: false, foreign_key: true
-
-      t.timestamps
     end
   end
 end
