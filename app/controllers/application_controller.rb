6170bdc66612fe6555478d9c128870a291d9cd0a
usbbush <jeanbienvenusb@gmail.com>
2023-06-28 11:14:29 +0200

    Tell devise to accept more datafields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar phone_number role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar phone_number role_id])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name avatar phone_number])
   end
 end
