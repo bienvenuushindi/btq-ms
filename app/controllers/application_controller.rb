e12bca020428afab1355e1805a5625db868a948e
usbbush <jeanbienvenusb@gmail.com>
2023-06-28 14:30:28 +0200

    Create a json-response-helper module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::API
   respond_to :json
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  def authenticate_user!
+    render json: { error: 'Not Authorized' }, status: :unauthorized unless user_signed_in? || request.method == 'POST'
+  end
 
   protected
 
