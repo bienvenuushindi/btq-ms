c7934da037704d243e6139c744e3ad51eda6e50b
usbbush <jeanbienvenusb@gmail.com>
2023-06-28 14:31:06 +0200

    Implement role model actions

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -1,2 +1,32 @@
-class RolesController
+class Api::V1::RolesController < ApplicationController
+  include JsonResponseHelper
+  skip_before_action :authenticate_user!, only: %i[]
+  before_action :set_role, only: %i[show]
+
+  def index
+    render json: fetch_response(Role.all), status: :ok
+  end
+
+  def create
+    role = Role.new(role_params)
+    if role.save
+      render json: created_response(role), status: :created
+    else
+      render json: error_response(role)
+    end
+  end
+
+  def show
+    render json: fetch_response(set_role), status: :ok
+  end
+
+  private
+
+  def set_role
+    Role.find(params[:id])
+  end
+
+  def role_params
+    params.require(:role).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,9 @@ Rails.application.routes.draw do
              }
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :roles, only: [:index, :create, :show]
+    end
+  end
 end
