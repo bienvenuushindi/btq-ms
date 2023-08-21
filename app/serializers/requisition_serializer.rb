4979b73c4bc872932a40dce7513cb736571ec3cc
usbbush <jeanbienvenusb@gmail.com>
2023-08-21 14:18:52 +0200

    Update serializer attributes

--- app/serializers/requisition_serializer.rb
+++ app/serializers/requisition_serializer.rb
@@ -5,8 +5,20 @@ class RequisitionSerializer
     object.product_details.with_attached_images.joins(:product).select('product_details.*, products.name')
   end
 
+  attribute :total_price do |object|
+    object[:total_price] || 0
+  end
+
+  attribute :price_currency do |object|
+    object[:price_currency] || '-'
+  end
+
+  attribute :archived do |object|
+    object[:archived] || false
+  end
+
   attribute :product_items do |object|
-    object.product_details.with_attached_images.joins(:product).select('product_details.*, products.name').map do |product_detail|
+    object.product_details.with_attached_images.joins(:product).select('product_details.*, products.name,product_details_requisitions.*').map do |product_detail|
       product_detail_hash = product_detail.as_json # Convert ProductDetail to hash and exclude images
       {
         **product_detail_hash, # Spread the attributes of product_detail
@@ -15,6 +27,7 @@ class RequisitionSerializer
     end
   end
 end
+
 # attribute :details  do |object|
 #   options={}
 #   options[:include] =['product']
