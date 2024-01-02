a85ea44a2ef079fa3d53c71e466c677af37590b6
usbbush <jeanbienvenusb@gmail.com>
2024-01-02 19:12:16 +0200

    Update product_items attributes

--- app/serializers/requisition_serializer.rb
+++ app/serializers/requisition_serializer.rb
@@ -18,19 +18,13 @@ class RequisitionSerializer
   end
 
   attribute :product_items do |object|
-    object.product_details.with_attached_images.joins(:product).select('product_details.*, products.name,product_details_requisitions.*').map do |product_detail|
+    object.product_details.joins(:product).select('product_details.*, products.name,product_details_requisitions.*').distinct.map do |product_detail|
       product_detail_hash = product_detail.as_json # Convert ProductDetail to hash and exclude images
       {
         **product_detail_hash, # Spread the attributes of product_detail
         image_urls: product_detail.image_urls, # Use the image_urls method from the ProductDetail model
+        quantity_type: ProductDetailRequisition.reverse_quantity_types[product_detail.quantity_type],
       }
     end
   end
 end
-
-# attribute :details  do |object|
-#   options={}
-#   options[:include] =['product']
-#   options[:fields] = { product: [:name, :details] }
-#   ProductDetailSerializer.new(object.product_details,options)
-# end
