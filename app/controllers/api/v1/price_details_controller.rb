4e012c335231e5d5de9580d0800386d976140c54
usbbush <jeanbienvenusb@gmail.com>
2024-01-02 19:28:48 +0200

    Use new serializer and update create action

--- app/controllers/api/v1/price_details_controller.rb
+++ app/controllers/api/v1/price_details_controller.rb
@@ -2,16 +2,18 @@ class Api::V1::PriceDetailsController < ApplicationController
   before_action :set_price_detail, only: %i[show]
 
   def index
-    data = PriceDetailSerializer.new(PriceDetail.where(product_detail_id: params[:product_detail_id]))
-    render json: data, status: :ok
+    prices = PriceDetail.where(product_detail_id: params[:product_detail_id])
+    grouped_by_supplier = PriceDetailSerializer.group_by_supplier(prices)
+    render json: grouped_by_supplier, status: :ok
   end
 
   def create
-    price_detail = PriceDetail.new(price_detail_params)
-    if price_detail.save
-      render json: created_response(price_detail), status: :created
+    prices_hash = price_detail_params[:prices].to_h
+    success = PriceDetail.custom_upsert(prices_hash, price_detail_params[:currency], price_detail_params[:supplier_id], price_detail_params[:product_detail_id])
+    if success
+      render json: { status: 'success' }, status: :ok
     else
-      render json: error_response(price_detail)
+      render json: { status: 'error' }, status: :unprocessable_entity
     end
   end
 
@@ -21,11 +23,15 @@ class Api::V1::PriceDetailsController < ApplicationController
 
   private
 
+  def serializer
+    PriceDetailSerializer
+  end
+
   def set_price_detail
     PriceDetail.find(params[:id])
   end
 
   def price_detail_params
-    params.require(:price_detail).permit(:dozen, :box, :currency, :supplier_id, :product_detail_id)
+    params.require(:price_detail).permit(:supplier_id, :currency, :product_detail_id, prices: [:box, :dozen, :unit])
   end
 end
