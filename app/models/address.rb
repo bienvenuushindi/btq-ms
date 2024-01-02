a2942fb777abd49357ab769bf155e3e1ec846d63
usbbush <jeanbienvenusb@gmail.com>
2024-01-02 19:14:23 +0200

    Add supplier address scope

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
   belongs_to :country
-
   validates :line1, presence: true
   validates :city, presence: true
   validates :phone_number1, presence: true
+  scope :supplier_addresses, -> { where(addressable_type: 'Supplier') }
 end
