fa319a6681d908aa3df7a53f08526a682b0f2182
usbbush <jeanbienvenusb@gmail.com>
2024-01-02 19:10:07 +0200

    Add attributes to product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,7 +1,17 @@
 class ProductSerializer < Serializer
   has_many :product_details
-  attributes :name, :short_description, :description, :active, :country_origin, :created_at, :updated_at, :image_urls
+  has_many :tags
+  attributes :name, :short_description, :description, :active, :country_origin, :updated_at, :image_urls
   attribute :details do |object|
     object.product_details.select("id, (size || '- $' || unit_price) as size_price").collect { |t| {id: t.id, name: "#{object.name} #{t.size_price}"} }
+  end
+  attribute :tags do |object|
+    object.tags.map { |tag| tag['name'] }
+  end
+  attribute :categories do |object|
+    object.categories.select("name")
+ end
+  attribute :created_at do |object|
+    object.created_at.strftime("%B %-d, %Y")
   end
 end
