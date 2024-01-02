73b9fe6ffa8d9260f187e1dc428a8cce2359fa88
usbbush <jeanbienvenusb@gmail.com>
2024-01-02 19:35:00 +0200

    Add paginable concern

--- app/controllers/concerns/paginable.rb
+++ app/controllers/concerns/paginable.rb
@@ -1,27 +1,29 @@
 # app/controllers/concerns/paginable.rb
 module Paginable
-    extend ActiveSupport::Concern
-    
-    def paginator
-      JSOM::Pagination::Paginator.new
-    end
-    
-    def pagination_params
-      params.permit![:page] # defaults to 20 pages 
-    end
-    
-    def paginate(collection)
-      paginator.call(collection, params: pagination_params, base_url: request.url)
-    end
-  
-    def render_collection(paginated, opt=nil)
-      options = {
-        meta: paginated.meta.to_h, # Will get total pages, total count, etc.
-        links: paginated.links.to_h
-      }
-      options = options.merge(opt) unless opt.nil?
-      paginated_result = serializer.new(paginated.items, options)
-  
-      render json: paginated_result,  status: :ok
-    end
+  extend ActiveSupport::Concern
+
+  def paginator
+    JSOM::Pagination::Paginator.new
+  end
+
+  def pagination_params
+    params.permit![:page] # defaults to 20 pages
+  end
+
+  def paginate(collection)
+    params = pagination_params || { number: 1, size: 10 };
+
+    paginator.call(collection, params:, base_url: request.url)
+  end
+
+  def render_collection(paginated, opt = nil)
+    options = {
+      meta: paginated.meta.to_h, # Will get total pages, total count, etc.
+      links: paginated.links.to_h
+    }
+    options = options.merge(opt) unless opt.nil?
+    paginated_result = serializer.new(paginated.items, options)
+
+    render json: paginated_result, status: :ok
+  end
 end
