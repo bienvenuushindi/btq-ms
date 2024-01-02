0912ebb1037833158280df9b1e0f9c6764487588
usbbush <jeanbienvenusb@gmail.com>
2024-01-02 19:19:44 +0200

    Add foreign key name explicitly

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,8 +1,7 @@
 class Country < ApplicationRecord
-  has_many :addresses
+  has_many :addresses, foreign_key: :country_id
   has_many :suppliers, through: :addresses, source: :addressable, source_type: 'Supplier'
   has_many :users, through: :addresses, source: :addressable, source_type: 'User'
-
   validates :name, presence: true, uniqueness: true
   validates :code, presence: true, uniqueness: true
 end
