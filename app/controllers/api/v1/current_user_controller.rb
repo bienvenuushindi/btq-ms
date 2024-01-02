51f9940e00ba1ce784c862089578fba15e0e550f
usbbush <jeanbienvenusb@gmail.com>
2024-01-02 19:06:27 +0200

    Apply  serializer

--- app/controllers/api/v1/current_user_controller.rb
+++ app/controllers/api/v1/current_user_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::CurrentUserController < ApplicationController
   def index
-    render json: current_user, status: :ok
+    data = serializer.new(current_user)
+    render json: data, status: :ok
+  end
+
+  def serializer
+    UserSerializer
   end
 end
