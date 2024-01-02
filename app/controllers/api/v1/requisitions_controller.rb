88d82e087d3a3308d126c74b3458d7c43957da4b
usbbush <jeanbienvenusb@gmail.com>
2024-01-02 19:31:56 +0200

    Implement update record and filters  action

--- app/controllers/api/v1/requisitions_controller.rb
+++ app/controllers/api/v1/requisitions_controller.rb
@@ -2,19 +2,25 @@ class Api::V1::RequisitionsController < ApplicationController
   before_action :set_requisition, only: %i[show]
 
   def index
-    options= {}
+    options = {}
     options[:fields] = { requisition: [:total_price, :count_products, :count_products_bought, :price_currency, :archived, :date] }
-    render json: serializer.new(Requisition.all, options), status: :ok
+    requisitions = Requisition.all
+    requisitions = requisitions.search(params[:q]) if params[:q].present?
+    requisitions = requisitions.order(created_at: :desc)
+    paginated = paginate(requisitions)
+
+    requisitions.present? ? render_collection(paginated, options) : :not_found
   end
 
   def create
     requisition = Requisition.new(
       user: current_user,
-      date: requisition_params[:date]
+      date: requisition_params[:date],
+      price_currency: requisition_params[:currency]
     )
     if requisition.save
       options = {}
-      options[:include] = ['product_details']
+      # options[:include] = ['product_details']
       data = serializer.new(requisition, options)
       render json: data, status: :created
     else
@@ -22,8 +28,20 @@ class Api::V1::RequisitionsController < ApplicationController
     end
   end
 
-  def update
-
+  def update_products_list
+    prod_req = set_requisition.product_detail_requisitions.where(product_detail_id: params[:product_detail_id]).first
+    prod_req.decrement_total_price
+    result = prod_req.update(update_requisition_params)
+    if result
+      price = update_requisition_params[:price]
+      currency = update_requisition_params[:currency]
+      supplier_id = update_requisition_params[:supplier_id]
+      product_detail_id = params[:product_detail_id]
+      quantity_type = update_requisition_params[:quantity_type]
+      PriceDetail.custom_upsert({ quantity_type => price }, currency, supplier_id, product_detail_id)
+    end
+    data = serializer.new(set_requisition)
+    render json: data, status: :ok
   end
 
   def add_products
@@ -35,8 +53,7 @@ class Api::V1::RequisitionsController < ApplicationController
         requisition.product_details << product_detail
       end
     end
-    if requisition
-      data = serializer.new(requisition)
+    if (data = serializer.new(requisition))
       render json: data, status: :created
     else
       render json: error_response(requisition)
@@ -45,15 +62,19 @@ class Api::V1::RequisitionsController < ApplicationController
 
   def show
     options = {}
-    # options[:include] =['product_details']
-    data = serializer.new(set_requisition, options)
-    render json: data, status: :ok
+    render json: serializer.new(set_requisition, options), status: :ok
+  end
+
+  def find_by_date
+    date_to_search = Date.parse(params[:date])
+    records = Requisition.find_by_date(date_to_search)
+    render json: serializer.new(records), status: :ok
   end
 
   def remove_item
     requisition = Requisition.find(params[:id])
     product_detail = ProductDetail.find(params[:product_detail_id])
-     ProductDetailRequisition.where(requisition: requisition, product_detail: product_detail).destroy_all
+    ProductDetailRequisition.where(requisition: requisition, product_detail: product_detail).destroy_all
 
     head :no_content
   end
@@ -72,7 +93,11 @@ class Api::V1::RequisitionsController < ApplicationController
     requisition_params.fetch(:product_detail_ids, [])
   end
 
+  def update_requisition_params
+    params.require(:requisition_product).permit(:price, :currency, :found, :quantity, :quantity_type, :note, :supplier_id, :expired_date)
+  end
+
   def requisition_params
-    params.require(:requisition).permit(:date, product_detail_ids: [])
+    params.require(:requisition).permit(:date, :currency, product_detail_ids: [])
   end
 end
