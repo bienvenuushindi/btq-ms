f821691de36a9aefb3f2cf6fd07ab9c84cbd25ff
usbbush <jeanbienvenusb@gmail.com>
2024-01-26 20:28:53 +0200

    create language attributes

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module BtqMs
     # config.session_store :cookie_store, key: '_interslice_session'
     # config.middleware.use ActionDispatch::Cookies
     # config.middleware.use config.session_store, config.session_options
+    config.i18n.default_locale = :en
+
   end
 end
