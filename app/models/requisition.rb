4c5561e72db6b3d2d850fd2160035b158254f710
usbbush <jeanbienvenusb@gmail.com>
2024-03-15 16:53:02 +0200

    Add method to handle recent entities

--- app/models/requisition.rb
+++ app/models/requisition.rb
@@ -5,4 +5,19 @@ class Requisition < ApplicationRecord
   has_many :product_detail_requisitions
   has_many :product_details, through: :product_detail_requisitions
 
+  scope :archived, -> { where(archived: true) }
+  scope :active, -> { where(archived: nil) }
+  scope :most_recent_archived, -> { archived.order(created_at: :desc).limit(1) }
+  scope :most_recent_active, -> { active.order(created_at: :desc).limit(1) }
+
+  scope :select_home_result, -> { select(:id, :total_price, :count_products_bought, :count_products, :price_currency, :date) }
+
+  def self.most_recent_requisitions
+    archived_result = most_recent_archived&.select_home_result&.first
+    active_result = most_recent_active&.select_home_result&.first
+
+    { active: active_result || {}, archived: archived_result || {} }
+  end
+
+
 end
