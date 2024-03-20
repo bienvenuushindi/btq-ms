6043b341ce89a6db6fe162456250a996a7946000
usbbush <jeanbienvenusb@gmail.com>
2024-03-20 12:27:10 +0200

    Add id attribute

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,7 +1,7 @@
 class ProductSerializer < Serializer
   has_many :product_details
   has_many :tags
-  attributes :name, :short_description, :description, :active, :country_origin, :updated_at, :image_urls
+  attributes :id, :name, :short_description, :description, :active, :country_origin, :updated_at, :image_urls
   attribute :details do |object|
     object.product_details.select("id, (size || '- $' || unit_price) as size_price").collect { |t| {id: t.id, name: "#{object.name} #{t.size_price}"} }
   end
--- app/serializers/requisition_serializer.rb
+++ app/serializers/requisition_serializer.rb
@@ -1,5 +1,5 @@
 class RequisitionSerializer < Serializer
-  attributes :total_price, :count_products, :count_products_bought, :price_currency, :archived, :date
+  attributes :id, :total_price, :count_products, :count_products_bought, :price_currency, :archived, :date
   attribute :product_items do |object|
     object.product_details.with_attached_images.joins(:product).select('product_details.*, products.name')
   end
