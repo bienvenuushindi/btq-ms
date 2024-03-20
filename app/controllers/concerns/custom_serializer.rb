70b223e83099b605b5fe5c42a4f0c97c39ba9902
usbbush <jeanbienvenusb@gmail.com>
2024-03-20 12:28:50 +0200

    Wrap result with data key

--- app/controllers/concerns/custom_serializer.rb
+++ app/controllers/concerns/custom_serializer.rb
@@ -4,10 +4,10 @@ module CustomSerializer
   extend ActiveSupport::Concern
 
   def serialize_resource(resource, serializer_class, options = {})
-    serializer_class.new(resource, options).serializable_hash[:data][:attributes]
+    { data: serializer_class.new(resource, options).serializable_hash[:data][:attributes]}
   end
 
   def serialize_resources(resources, serializer_class, options = {})
-    serializer_class.new(resources, options).serializable_hash[:data].map { |data| data[:attributes] }
+    {data: serializer_class.new(resources, options).serializable_hash[:data].map { |data| data[:attributes] }}
   end
 end
