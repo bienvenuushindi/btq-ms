b98ad0ea9fbe3568c0b5d29fc037cc151f2d42db
usbbush <jeanbienvenusb@gmail.com>
2024-04-08 16:11:39 +0200

    Remove comment

--- app/models/price_detail.rb
+++ app/models/price_detail.rb
@@ -2,8 +2,6 @@ class PriceDetail < ApplicationRecord
   belongs_to :supplier
   belongs_to :product_detail
   enum :quantity_type, { box: 0, dozen: 1, unit: 2 }
-  # validates :dozen, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
-  # validates :box, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
   validates :currency, presence: true
 
   def self.custom_upsert(prices, currency, supplier_id, product_detail_id)
