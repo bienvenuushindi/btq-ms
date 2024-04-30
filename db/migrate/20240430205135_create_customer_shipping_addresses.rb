a7116db390b7702c240ba1998e2ed36ed837fc87
usbbush <jeanbienvenusb@gmail.com>
2024-05-01 00:03:51 +0200

    Update shipping_address association

--- db/migrate/20240430205135_create_customer_shipping_addresses.rb
+++ db/migrate/20240430205135_create_customer_shipping_addresses.rb
@@ -2,6 +2,7 @@ class CreateCustomerShippingAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :customer_shipping_addresses do |t|
       t.references :individual_order, null: false, foreign_key: true
+      t.references :address, null: false, foreign_key: true
       t.timestamps
     end
   end
