a630b11e89e13e4b003fbb092e95efb7bdb14ea2
usbbush <jeanbienvenusb@gmail.com>
2024-04-30 22:07:36 +0200

    Make column  nullable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_03_144654) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_10_193816) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
