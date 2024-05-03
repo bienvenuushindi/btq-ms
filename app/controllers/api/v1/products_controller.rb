855787defae27b0c34ce08a71cec4a27b7598904
usbbush <jeanbienvenusb@gmail.com>
2024-05-04 00:51:09 +0200

    Add service for product creation to separate concern

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,64 +1,27 @@
 class Api::V1::ProductsController < ApplicationController
+  include CategoryHelper
   before_action -> { find_record(Product) }, only: %i[show update]
 
   def index
-    products = Product.all
-    products = products.search(params[:q]) if params[:q].present?
-    products = products.by_status(params[:status]) if params[:status].present?
-    products = products.reorder(sort_column => sort_direction)
-    products = products.with_attached_images.order(created_at: :desc)
-    paginated = paginate(products)
-
-    if products.present?
-      render_collection(paginated,serializer)
-    else
-      render json: { error: 'No products found' }, status: :not_found
-    end
+    render_collection(paginated_products, serializer)
   end
 
   def count_by_status
     active_count = Product.count_by_status(true)
     inactive_count = Product.count_by_status(false)
 
-    render json: {data: {
+    render json: { data: {
       active: active_count,
       inactive: inactive_count
-    }}, status: :ok
-  end
-
-  def sort_column
-    %w[name active created_at country_origin].include?(params[:sort]) ? params[:sort] : 'created_at'
-  end
-
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'desc'
+    } }, status: :ok
   end
 
   def search
-    products = nil
-    paginated = nil
-    options = {}
-    if params[:q].present?
-      options[:fields] = { product: %i[name details] }
-      products = Product.all.search(params[:q])
-      products = products.order(created_at: :desc)
-      paginated = paginate(products)
-    end
-    if products.present?
-      render_collection(paginated, serializer, options)
-    else
-      render json: { error: 'No products found' }, status: :not_found
-    end
+    render_collection(paginated_search_results, serializer, search_options)
   end
 
   def create
-    @product = build_product_from_params
-
-    if @product.save
-      render json: serialize_resource(@product, serializer), status: :created
-    else
-      render json: error_response(@product, 'Failed to create the product'), status: :unprocessable_entity
-    end
+    create_product
   end
 
   def show
@@ -69,7 +32,6 @@ class Api::V1::ProductsController < ApplicationController
   def update
     Product.transaction do
       update_product_attributes(@product, product_params)
-
       if @product.save
         render json: serialize_resource(@product, serializer), status: :ok
       else
@@ -80,11 +42,47 @@ class Api::V1::ProductsController < ApplicationController
 
   private
 
+  def paginated_search_results
+    return unless params[:q].present?
+
+    products = Product.all.search(params[:q])
+    products = products.order(created_at: :desc)
+    paginated = paginate(products)
+
+    paginated.present? ? paginated : { error: 'No products found' }
+  end
+
+  def search_options
+    { fields: { product: %i[name details] } }
+  end
+
+  def sort_column
+    %w[name active created_at country_origin].include?(params[:sort]) ? params[:sort] : 'created_at'
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'desc'
+  end
+
+  def paginated_products
+    products = Product.all
+    products = products.search(params[:q]) if params[:q].present?
+    products = products.by_status(params[:status]) if params[:status].present?
+    products = products.reorder(sort_column => sort_direction)
+    products = products.with_attached_images.order(created_at: :desc)
+    paginate(products)
+  end
+
   def update_product_attributes(product, params)
-    attributes_to_update = %i[name short_description description active country_origin tags images]
+    attributes_to_update = %i[name short_description description active country_origin tags images categories]
 
     attributes_to_update.each do |attribute_name|
-      update_attribute(product, attribute_name, params[attribute_name])
+      if attribute_name.to_sym == :categories && params[attribute_name].present?
+        categories = parse_category_ids(params[attribute_name])
+        update_categories(product, categories)
+      else
+        update_attribute(product, attribute_name, params[attribute_name])
+      end
     end
   end
 
@@ -92,19 +90,16 @@ class Api::V1::ProductsController < ApplicationController
     ProductSerializer
   end
 
-  def build_product_from_params
-    Product.new(
-      user: current_user,
-      name: product_params[:name],
-      short_description: product_params[:short_description],
-      description: product_params[:description],
-      active: product_params[:active],
-      country_origin: product_params[:country_origin],
-      images: product_params[:images]
-    ).tap { |product| product.tag_list = product_params[:tags] unless product_params[:tags].blank? }
+  def create_product
+    @product = ProductService::Creator.new(product_params, current_user).call
+    if @product.persisted?
+      render json: serialize_resource(@product, serializer), status: :created
+    else
+      render json: error_response(@product), status: :unprocessable_entity
+    end
   end
 
   def product_params
-    params.require(:product).permit(:name, :short_description, :description, :active, :country_origin, :tags, images: [])
+    params.require(:product).permit(:name, :short_description, :description, :active, :country_origin, :tags, :categories, images: [])
   end
 end
--- app/services/product_service/creator.rb
+++ app/services/product_service/creator.rb
@@ -1,4 +1,38 @@
 # frozen_string_literal: true
+module ProductService
+  class Creator < Base::Creator
+    include CategoryHelper
 
-class Creator
+    def initialize(params, user)
+      @current_user = user
+      super(params)
+    end
+
+    private
+
+    def create_record
+      @product = build_product_with_tags
+      @product.save!
+      add_categories
+
+      @product
+    end
+
+    def build_product_with_tags
+      @product = Product.new(
+        user: @current_user,
+        name: @params[:name],
+        short_description: @params[:short_description],
+        description: @params[:description],
+        active: @params[:active],
+        country_origin: @params[:country_origin],
+        images: @params[:images]
+      ).tap { |product| product.tag_list = @params[:tags] unless @params[:tags].blank? }
+    end
+
+    def add_categories
+      categories = parse_category_ids(@params[:categories])
+      update_categories(@supplier, categories)
+    end
+  end
 end
