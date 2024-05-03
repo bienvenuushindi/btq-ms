7be52ddbaae4bc277cea7682d87a870bb41b32d4
usbbush <jeanbienvenusb@gmail.com>
2024-05-04 00:52:25 +0200

    Update autoloads configs

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module BtqMs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # config.autoloader = :zeitwerk
+    config.autoload_paths += %W(#{config.root}/app/services)
+    config.autoload_paths += %W(#{config.root}/app/services/**/)
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
