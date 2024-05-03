b793b3a007f841340c6ed654b33c37e76c4c0f0d
usbbush <jeanbienvenusb@gmail.com>
2024-05-04 00:47:28 +0200

    Create module to handle activerecord exceptions

--- app/controllers/concerns/error_handler.rb
+++ app/controllers/concerns/error_handler.rb
@@ -1,24 +1,25 @@
-# frozen_string_literal: true
-
+# app/controllers/concerns/error_handler.rb
 module ErrorHandler
   extend ActiveSupport::Concern
 
   included do
-    rescue_from ActiveRecord::RecordInvalid, with: :handle_validation_error
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
     rescue_from StandardError, with: :handle_unexpected_error
   end
 
-  def handle_validation_error(exception)
-    # Handle validation errors
-    Rails.logger.error("Validation error: #{exception.message}")
-    # Add more specific error handling or notifications if needed
-    render json: { error: exception.message }, status: :unprocessable_entity
+  def record_invalid(exception)
+    message = exception.message.partition('Validation failed: ').last
+    render json: { meta: { message: message } }, status: 401
+  end
+
+  def record_not_found(exception)
+    model_name = exception.model.constantize.model_name.human
+    render json: { "#{model_name}": nil }, status: :not_found
   end
 
   def handle_unexpected_error(exception)
-    # Handle unexpected errors
     Rails.logger.error("Unexpected error: #{exception.message}")
-    # Add more specific error handling or notifications if needed
     render json: { error: 'Something went wrong' }, status: :internal_server_error
   end
 end
