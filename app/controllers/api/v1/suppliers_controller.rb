15b63dc6daa909720b0441c224b76f0d015c4d8b
usbbush <jeanbienvenusb@gmail.com>
2024-05-04 00:51:49 +0200

    Use supplier service  creator

--- app/controllers/api/v1/suppliers_controller.rb
+++ app/controllers/api/v1/suppliers_controller.rb
@@ -1,71 +1,69 @@
 class Api::V1::SuppliersController < ApplicationController
+  include CategoryHelper
   before_action -> { find_record(Supplier) }, only: %i[show update]
 
   def index
-    options = {}
-    options[:fields] = { supplier: [:id, :shop_name, :image_urls, :address] }
-    suppliers = Supplier.all
-    suppliers = suppliers.search(params[:q]) if params[:q].present?
-    suppliers = suppliers.reorder(sort_column => sort_direction)
-    suppliers = suppliers.with_attached_images.order(created_at: :desc)
-    paginated = paginate(suppliers)
-
-   render_collection(paginated, serializer, options)
-  end
-
-
-  def sort_column
-    %w{shop_name created_at}.include?(params[:sort]) ? params[:sort] : "created_at"
-  end
-
-  def sort_direction
-    %w{asc desc }.include?(params[:direction]) ? params[:direction] : "desc"
+    render_collection(paginated_suppliers, serializer, index_options)
   end
 
   def search
-    suppliers = nil
-    paginated = nil
-    options = {}
-    if params[:q].present?
-      options[:fields] = { supplier: [:id, :shop_name, :image_urls, :address] }
-      suppliers = Supplier.all.search(params[:q]).limit(4)
-      if params[:product_detail_id].present?
-        prod_supplier_ids = ProductDetail.find_by(id: params[:product_detail_id]).suppliers.pluck(:id)
-        # Exclude suppliers in prod_suppliers
-        suppliers = suppliers.where.not(id: prod_supplier_ids)
-      end
-      suppliers = suppliers.order(created_at: :desc)
-      paginated = paginate(suppliers)
-    end
-     render_collection(paginated, serializer, options)
-
+    render_collection(paginated_search_results, serializer, search_options)
   end
 
   def create
-    @supplier = Supplier.new(shop_name: supplier_params[:shop_name], user: current_user, images: supplier_params[:images])
-    @supplier.tag_list = supplier_params[:tags] unless supplier_params[:tags].blank?
-    if @supplier.save
-      country = Country.create_with(name: supplier_params[:country_name]).find_or_create_by(code: supplier_params[:country_id])
-      Address.create!(line1: supplier_params[:address1],
-                      city: supplier_params[:city],
-                      country:,
-                      phone_number1: supplier_params[:tel1],
-                      phone_number2: supplier_params[:tel2],
-                      line2: supplier_params[:address2],
-                      addressable: @supplier)
-      render json: serialize_resource(@supplier, serializer), status: :created
-    else
-      render json: error_response(@supplier)
-    end
+    create_supplier
   end
 
   def show
-    options = {}
-    options[:fields] = { supplier: [:id, :shop_name, :tags, :image_urls, :address1, :address2, :city, :country, :code, :tel1, :tel2] }
     render json: serialize_resource(@supplier, serializer), status: :ok
   end
 
   def update
+    update_supplier
+  end
+
+  private
+
+  def paginated_suppliers
+    suppliers = Supplier.all
+    suppliers = suppliers.search(params[:q]) if params[:q].present?
+    suppliers = suppliers.reorder(sort_column => sort_direction)
+    suppliers = suppliers.with_attached_images.order(created_at: :desc)
+    paginate(suppliers)
+  end
+
+  def sort_column
+    %w[shop_name created_at].include?(params[:sort]) ? params[:sort] : "created_at"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+  end
+
+  def paginated_search_results
+    return unless params[:q].present?
+
+    suppliers = Supplier.all.search(params[:q]).limit(4)
+    filter_suppliers_by_product_detail(suppliers) if params[:product_detail_id].present?
+    suppliers = suppliers.order(created_at: :desc)
+    paginate(suppliers)
+  end
+
+  def filter_suppliers_by_product_detail(suppliers)
+    prod_supplier_ids = ProductDetail.find_by(id: params[:product_detail_id]).suppliers.pluck(:id)
+    suppliers.where.not(id: prod_supplier_ids)
+  end
+
+  def create_supplier
+    @supplier = SupplierService::Creator.new(supplier_params, current_user).call
+    if @supplier.persisted?
+      render json: serialize_resource(@supplier, serializer), status: :created
+    else
+      render json: error_response(@supplier), status: :unprocessable_entity
+    end
+  end
+
+  def update_supplier
     update_supplier_attributes(@supplier, supplier_params)
     if @supplier.save
       render json: serialize_resource(@supplier, serializer), status: :ok
@@ -73,13 +71,22 @@ class Api::V1::SuppliersController < ApplicationController
       render json: error_response(@supplier), status: :unprocessable_entity
     end
   end
-  
-  private
+
   def update_supplier_attributes(supplier, params)
     update_attribute(supplier, :shop_name, params[:shop_name])
     update_attribute(supplier, :tags, params[:tags])
     update_attribute(supplier, :images, params[:images])
     update_attribute(supplier, :address, params)
+    categories = parse_category_ids(params[:categories])
+    update_categories(supplier, categories)
+  end
+
+  def index_options
+    { fields: { supplier: [:id, :shop_name, :image_urls, :address] } }
+  end
+
+  def search_options
+    { fields: { supplier: [:id, :shop_name, :image_urls, :address, :categories] } }
   end
 
   def serializer
@@ -87,6 +94,6 @@ class Api::V1::SuppliersController < ApplicationController
   end
 
   def supplier_params
-    params.require(:supplier).permit(:shop_name, :address1, :address2, :city, :tel1, :country_name, :tel2, :country_id, :tags, images: [])
+    params.require(:supplier).permit(:shop_name, :address1, :address2, :city, :tel1, :country_name, :tel2, :country_id, :tags, :categories, images: [])
   end
 end
