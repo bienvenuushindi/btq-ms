bd4832e3787a4b0cc0d8adba7249a9541d8a4194
usbbush <jeanbienvenusb@gmail.com>
2024-05-04 21:18:08 +0200

    Implement a base retriever base class

--- app/services/base/retriever.rb
+++ app/services/base/retriever.rb
@@ -1,4 +1,31 @@
 # frozen_string_literal: true
+module Base
+  class Retriever < ApplicationService
+    attr_accessor :sort_column
 
-class Retriever
+    def initialize(scope, filter_params)
+      @scope = scope
+      @params = filter_params
+      @sort_direction = %w[asc desc]
+    end
+
+    def call
+      apply_filters
+      apply_sorting
+    end
+
+    private
+
+    def apply_filters
+      @scope = @scope.search(@params[:q]) if @params[:q].present?
+      @scope = @scope.by_status(@params[:status]) if @params[:status].present?
+      # Add any additional product-specific filtering logic here based on @params
+    end
+
+    def apply_sorting
+      sort_column = @sort_column.include?(@params[:sort]) ? @params[:sort] : 'created_at'
+      sort_direction = @sort_direction.include?(@params[:direction]) ? @params[:direction] : 'desc'
+      @scope = @scope.reorder(sort_column => sort_direction)
+    end
+  end
 end
