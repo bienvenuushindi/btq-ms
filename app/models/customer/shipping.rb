c36ff614bde3021cb7cdbaf45b69ae3acaa03af6
usbbush <jeanbienvenusb@gmail.com>
2024-07-11 01:08:06 +0200

    Fix unknown key error

--- app/models/customer/shipping.rb
+++ app/models/customer/shipping.rb
@@ -1,5 +1,5 @@
 class Customer::Shipping < ApplicationRecord
   belongs_to :individual_order
-  belongs_to :address, -> { shipping_address }, as: :addressable,  class_name: 'Address'
+  belongs_to :address, -> { shipping_address },  class_name: 'Address'
   delegate :line1, :city, :phone_number1, to: :address, prefix: false
 end
