b984a5ee066d54b2e1b10694338156bf027ad12d
usbbush <jeanbienvenusb@gmail.com>
2024-07-11 01:21:03 +0200

    Remove duplicate class model

--- app/models/customer/shipping_address.rb
+++
@@ -1,5 +0,0 @@
-class Customer::Shipping < ApplicationRecord
-  belongs_to :individual_order
-  belongs_to :address, -> { shipping_address },  class_name: 'Address'
-  delegate :line1, :city, :phone_number1, to: :address, prefix: false
-end
